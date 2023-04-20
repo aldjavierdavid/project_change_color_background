de2cd653b4f03c0bcc6395c194c928076bb0d69a
javier aldana <jdad1701@gmail.com>
2023-04-20 12:44:10 +0200

    ejercicio random bgr completado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,56 @@
 //En este array de strings, tenemos todos los colores
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+let colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+let color = document.querySelector('#bgr')
 
+    ;
+document.querySelector('button').onclick =
+
+    function () {
+        let randomColor = Math.floor(Math.random() * colors.length)
+        let colorGanador = colors[randomColor];
+        return document.body.style.backgroundColor = colorGanador
+
+
+    }
+
+
+
+
+/*function () {
+}
+
+let color = document.querySelector('#bgr')
+let color2 = color.style.backgroundColor
+
+if (color2 === 'purple') {
+    color.style.backgroundColor = "blue";
+
+}
+
+else if (color2 === "blue") {
+    color.style.backgroundColor = "green";
+
+}
+
+else if (color2 === "green") {
+    color.style.backgroundColor = "red";
+
+}
+
+else if (color2 === "red") {
+    color.style.backgroundColor = "pink";
+
+}
+
+else if (color2 === "pink") {
+    color.style.backgroundColor = "yellow";
+
+}
+
+
+else {
+    color.style.backgroundColor = "purple";
+
+}
+}*/
 
